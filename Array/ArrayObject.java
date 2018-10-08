ab29c58ba229402c368495ee37f1e5574fc8be9c
Jacky Satpal <jackysatpal.212@gmail.com>
2018-10-08 18:52:39 -0400

    updated code

--- Array/ArrayObject.java
+++ Array/ArrayObject.java
@@ -4,14 +4,20 @@ public class ArrayObject {
     public static void main(String[] args) {
         Array students = new Array(5);
 
-        students.add("Jacky");
-        students.addAtIndex(3, 456);
-        //students.add("Jacky");
-        //students.add("Jacky");
+        students.add(0,1);
+        students.add(1,2);
+        students.add(2,3);
+        students.add(3,4);
+        students.add(4,5);
+        students.print();
+        System.out.println(students.size());
 
-        students.printElements();
+        students.delete(2);
+        students.print();
+        System.out.println(students.size());
 
-        students.removeAtIndex(0);
-        students.printElements();
+        students.delete(7);
+        students.print();
+        System.out.println(students.size());
     }
 }
