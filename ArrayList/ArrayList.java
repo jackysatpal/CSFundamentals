32acdffa8ae35c3cd46e608c6f15edb933bdf68b
Jacky Satpal <jackysatpal.212@gmail.com>
2018-10-18 12:33:53 -0400

    modified after code review, have to implement Generic

--- ArrayList/ArrayList.java
+++ ArrayList/ArrayList.java
@@ -2,41 +2,42 @@ package ArrayList;
 
 import java.util.Arrays;
 
-public class ArrayList{
-    private final int DEFAULT_CAPACITY = 10;
+public class ArrayList {
+    private static final int DEFAULT_CAPACITY = 10;
     private Object[] elements;
-    private int index = 0;
     private int count = 0;
 
     public ArrayList(){
         this.elements = new Object[this.DEFAULT_CAPACITY];
     }
 
-    public void add(Object element){
-        if (this.index == this.elements.length)
+    public void add(Object element) {
+        if (this.count == this.elements.length) {
             ensureCapacity();
+        }
 
-        this.elements[index] = element;
-        this.index++;
+        this.elements[count] = element;
         this.count++;
     }
 
     public void add(int index, Object element) {
-        if (this.index == this.elements.length)
+        if (this.count == this.elements.length) {
             ensureCapacity();
+        }
 
         this.elements[index] = element;
         this.count++;
     }
 
-    public void set(int index, Object element){
-        if (index >= 0 && index < this.elements.length)
+    public void set(int index, Object element) {
+        if (index >= 0 && index < this.elements.length) {
             this.elements[index] = element;
-        else
+        } else {
             throw new IndexOutOfBoundsException();
+        }
     }
 
-    public void ensureCapacity(){
+    public void ensureCapacity() {
         int newSize = ((this.elements.length * 3) / 2) + 1;
         this.elements = Arrays.copyOf(this.elements, newSize);
     }
@@ -49,44 +50,44 @@ public class ArrayList{
         return this.count;
     }
 
-    public void clear(){
-        for (int i = 0; i < this.count; i++)
-            this.elements[i] = null;
-
+    public void clear() {
+        this.elements = new Object[0];
         this.count = 0;
-        this.index = 0;
     }
 
-    public boolean contains(Object o){
+    public boolean contains(Object o) {
         for (int i = 0; i < this.elements.length; i++) {
-            if (this.elements[i] == o || this.elements[i].equals(o))
+            if (this.elements[i] == o || this.elements[i].equals(o)) {
                 return true;
+            }
         }
 
         return false;
     }
 
-    public Object get(int index){
-        if (index >= 0 && index < this.elements.length)
-            return this.elements[index];
-        else
-            throw new IndexOutOfBoundsException();
-    }
-
-    public void remove(int index){
+    public Object get(int index) {
         if (index >= 0 && index < this.elements.length) {
-            for (int i = index; i < this.elements.length - 1; i++)
-                this.elements[i] = this.elements[i + 1];
-
-            this.count--;
-        } else
+            return this.elements[index];
+        } else {
             throw new IndexOutOfBoundsException();
+        }
     }
 
-    public void printElements(){
-        for (Object i : this.elements)
-            System.out.print(i + " ");
+    public void remove(int index) {
+        if (index >= 0 && index < this.elements.length) {
+            for (int i = index; i < this.elements.length - 1; i++) {
+                this.elements[i] = this.elements[i + 1];
+            }
+            this.count--;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
 
+    public void printElements() {
+        for (Object i : this.elements) {
+            System.out.print(i + " ");
+        }
         System.out.println();
     }
 }
